d6bcbc1b088d4808077f304c8bc3ef5f39c59fd2
MohammadYehya <mohammad7446@gmail.com>
2025-07-20 17:16:26 +0500

    Create m04-MessagePlaceholder.py

---
+++ m04-MessagePlaceholder.py
@@ -0,0 +1,19 @@
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+chat_template = ChatPromptTemplate([
+    ('system', 'You are a helpful customer support agent.'),
+    MessagesPlaceholder(variable_name='chat_history'),
+    ('human', '{query}')
+])
+
+chat_history = []
+with open('m04-MessagePlaceholder.txt') as f:
+    chat_history.extend(f.readlines())
+
+# print(chat_history)
+
+prompt = chat_template.invoke({'chat_history': chat_history, 'query': 'Where is my refund?'})
+
+print(prompt)
+
+# Usually when a user opens an old chat, we need to load the old messages and add them to the chat_history. For this a MessagePlaceholder is used
