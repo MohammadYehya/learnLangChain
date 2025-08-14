03d456905c92aa347bbd6f033c1a9db925cbb459
MohammadYehya <mohammad7446@gmail.com>
2025-08-14 23:06:17 +0500

    Create m12.2-Retrievers_Wikipedia.py

---
+++ m12.2-Retrievers_Wikipedia.py
@@ -0,0 +1,6 @@
+from langchain_community.retrievers import WikipediaRetriever
+
+retriever = WikipediaRetriever(top_k_results=2, lang='en')
+query = "The geopolitical history of India and Pakistan from the perspective of the Chinese."
+docs = retriever.invoke(query)
+print(docs)
